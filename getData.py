03d7efd63f725fb7827782ca1b67ca17c2599354
Ferdian Akbar <skykipe@student.telkomuniversity.ac.id>
2018-07-05 18:37:52 +0700

    Update getData.py

--- getData.py
+++ getData.py
@@ -6,10 +6,11 @@ import datetime
 now = datetime.datetime.now()
 date =now.strftime("%Y-%m-%d")
 
-token='eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2RlFRS1giLCJhdWQiOiIyMkNMVFIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyaHIgcnNsZSIsImV4cCI6MTUxOTAxMzQ4MiwiaWF0IjoxNTE4NDMwMTY5fQ.Kx5CBJ0RFgoWW80VOMdDrXCjaHJKMZUXqaEkV1-zdLo'
+token='YOUR TOKEN'
+USER_ID = 'YOUR USERID'
 
 #fill user id with your user id
-url = 'https://api.fitbit.com/1.2/user/"user id"/sleep/list.json?afterDate='+ date +'&limit=1&offset=0&sort=asc'
+url = 'https://api.fitbit.com/1.2/user/'+USER_ID+'/sleep/list.json?afterDate='+ date +'&limit=1&offset=0&sort=asc'
 
 filename = 'SleepData'+ date +'.json'
 response = requests.get(url=url, headers ={'Authorization':'Bearer ' + token})
